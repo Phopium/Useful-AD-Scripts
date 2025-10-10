8e4fecdbd5d46cf2f4e4d1bce21407d0826739f9
Phopium <fopis@hotmail.com>
2025-10-09 21:19:28 -0700

    Add GetUserGroups.ps1

---
+++ GetUserGroups.ps1
@@ -0,0 +1,7 @@
+﻿$upn = "user.name@YourDomain.com"
+$server = "YourServer.YourDomain.com"
+$groups = Get-ADUser -Filter {UserPrincipalName -eq $UPN} -server $server | Get-ADPrincipalGroupMembership -server $server | Select-Object Name
+
+# Adds ";" after each group for easy pasting back AD
+$list = $groups | ForEach-Object { $_.Name + ";" }
+Write-Output $list
